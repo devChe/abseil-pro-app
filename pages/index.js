90a788b267023b58e55f9a12c348e7dcbb85fd69
devChe <eugeniostevenson26@gmail.com>
2022-02-11 04:25:16 +0800

    add h4

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <>
       <div className='main'>
-        <h1 style={{ padding: "100px 0 20px" }}>Abseil Pro</h1>
+        <h1 style={{ padding: "100px 0 20px" }}>ABSEIL PRO</h1>
+        <h4>ROPE ACCESS & HEIGHT SAFETY</h4>
         <div style={{ display: "grid", width: "500px", margin: "0 auto" }}>
           <input  type="email" placeholder="E-mail" style={{marginBottom: "12px"}} />
           <input  type="password" placeholder="Password" />
