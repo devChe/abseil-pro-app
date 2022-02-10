5179e2e3d5c07b4045d4200cd48149d1dbdde1b6
devChe <eugeniostevenson26@gmail.com>
2022-02-11 04:10:17 +0800

    create auth

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+      <>
+        <div className='container'>
+            header
+        </div>
+      </>
+  )
+}
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
 import '../styles/globals.css'
+import 'react-skeleton-css/styles/skeleton.2.0.4.css';
+// normalize is also available
+import 'react-skeleton-css/styles/normalize.3.0.2.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Abreil Pro System" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='container' style={{ height:"90vh" }}>
+        <Component {...pageProps} />
+      </div>
+    </Layout>
+  )
 }
 
 export default MyApp
