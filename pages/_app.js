3d43c1b10e185dceef365b42fd02bcdb916d0620
devChe <eugeniostevenson26@gmail.com>
2022-02-18 04:07:15 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Layout from '../components/Layout'
 import '../styles/globals.css'
 import '../src/config/firebase.config'
 import 'react-skeleton-css/styles/skeleton.2.0.4.css'
-// normalize is also available
 import 'react-skeleton-css/styles/normalize.3.0.2.css'
 import { auth } from '../src/config/firebase.config'
 import { onAuthStateChanged, currentUser, signOut, signInWithEmailAndPassword } from 'firebase/auth';
