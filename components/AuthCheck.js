cf116cd6cab5cc4be721bc293103ef0119409f50
devChe <eugeniostevenson26@gmail.com>
2022-02-18 03:59:40 +0800

    create Auth Check Wrapper

---
+++ components/AuthCheck.js
@@ -0,0 +1,24 @@
+import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Loading from './Loading'
+import { auth } from '../src/config/firebase.config'
+
+export const AuthCheck = (props) => {
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [textError, setTextError] = useState("")
+    const [user, setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+  })
+  const router = useRouter()
+  // const user = useUser() // you need to implement this. In this example, undefined means things are still loading, null means user is not signed in, anything truthy means they're signed in
+  
+  if (typeof window !== 'undefined' && user === null) router.push('/');
+
+  if(!user) return <Loading /> // a loading component that prevents the page from rendering
+   
+  return props.children
+}
