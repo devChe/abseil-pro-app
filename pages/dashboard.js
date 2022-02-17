019f43d7d0358faadf35218c863b7f62b229282a
devChe <eugeniostevenson26@gmail.com>
2022-02-18 04:13:49 +0800

    delete modal component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -7,7 +7,6 @@ import { auth } from '../src/config/firebase.config'
 import { useState, useEffect } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Modal from '../components/modal'
 import { useRouter } from 'next/router'
 import { AuthCheck } from '../components/AuthCheck'
 
