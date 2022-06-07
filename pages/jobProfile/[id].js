b317a9492f39a89250799f4bd400b807a9d0434f
devChe <eugeniostevenson26@gmail.com>
2022-06-07 23:27:24 +0800

    return previous job

--- pages/jobProfile/[id].js
+++ pages/jobProfile/[id].js
@@ -210,12 +210,9 @@ function jobProfile({jobProps}) {
                                 <input type="hidden" value={url} onChange={(event) => {setUrl(event.target.value)}}  />
                                 <hr />
                                 <div className="container">
-                                    {job.images ? job.images.map(img => (
+                                    {job.images.map(img => (
                                         <img className="item" src={img.url} alt={img.name} width="200" height="200" />
-                                    )) : (
-                                        <h4 style={{ height:"20vh", whiteSpace:"nowrap"}}>No Images uploaded</h4>
-                                    
-                                    )
+                                    )) 
                                 }
                                        
                                 </div>
